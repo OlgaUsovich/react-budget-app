cef3f4deed41ce507425e24d5b98621ef392fd59
Olga <olga@gmail.com>
2022-08-10 22:47:32 +0300

    feat(hw-39): add button component layout

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  display: block;
+  margin-top: 10px;
+  padding: 15px 0;
+  border: 0;
+  border-radius: 10px;
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 400;
+  background-color: #23c9ff;
+  cursor: pointer;
+`;
